3f0368a7d6eb3615175ebc7cdf49cb6c95a66fe8
William Barksdale <wfbarksdale@gmail.com>
2022-01-26 15:59:28 -0500

    cleanup

--- tapestry/timp/src/index.ts
+++ tapestry/timp/src/index.ts
@@ -1,4 +1,3 @@
-import { writeFile } from "fs/promises";
 import Jimp from "jimp"
 
 const PATCH_WIDTH_PX = 24;
@@ -6,20 +5,12 @@ const PATCH_HEIGHT_PX = 24;
 
 export const processImage = async (buffer: Buffer, width: number, height: number) => {
     let image = await Jimp.read(buffer)
-    console.log("loaded image: ", image.getWidth(), image.getHeight());
-
-    // perform a crop of the image, such that it has the same aspect ratio as the desired output
-
     let aspectOutput = width / height;
     let aspectInput = image.getWidth() / image.getHeight();
     let imgCroppedHeight = image.getHeight();
     let imgCroppedWidth = image.getWidth();
 
-    console.log("Aspect in: ", aspectInput);
-    console.log("Aspect out: ", aspectOutput);
-
     if (aspectInput < aspectOutput) {
-        // input image has a greater height than the output, so reduce height
         imgCroppedHeight = image.getHeight() * (1 / aspectOutput);
         imgCroppedWidth = image.getWidth();
     } else if (aspectInput > aspectOutput) {
@@ -29,21 +20,15 @@ export const processImage = async (buffer: Buffer, width: number, height: number
         console.log("Aspects match, no initial crop needed");
     }
 
-    console.log("cropped: ", imgCroppedWidth, imgCroppedHeight);
     const xOffset = (image.getWidth() - imgCroppedWidth) / 2;
     const yOffset = (image.getHeight() - imgCroppedHeight) / 2;
 
-    let imgCropped = await image.crop(xOffset, yOffset, imgCroppedWidth, imgCroppedHeight);
+    // Crop the image so it matches the input aspect ratio
+    // then scale it so it maches the true pixel size of the patches
 
-    let imgCroppedData = await imgCropped.getBufferAsync("image/gif");
-    let imgCroppedResult = await writeFile("/tmp/imgcropped.gif", imgCroppedData);
-    // NOTE(will): imgCropped.writeAsync("/tmp/imgcroped.gif") seems to have a bug
-
-    // scale the image such that it will align with patch pixel boundaries
-
-    let imgScaled = await imgCropped.scale((width * PATCH_WIDTH_PX) / imgCroppedWidth)
-    let imgScaledData = await imgScaled.getBufferAsync("image/gif");
-    let imgScaledResult = await writeFile("/tmp/imgscaled.gif", imgScaledData);
+    let imgScaled = await image
+        .crop(xOffset, yOffset, imgCroppedWidth, imgCroppedHeight)
+        .scale((width * PATCH_WIDTH_PX) / imgCroppedWidth)
 
     // 2d array in row major order of patch image data
     let chunks: Buffer[][] = []
@@ -51,7 +36,6 @@ export const processImage = async (buffer: Buffer, width: number, height: number
     for (var y = 0; y < height; y++) {
         chunks.push([])
         for (var x = 0; x < width; x++) {
-            console.log(imgScaled.getWidth(), imgScaled.getHeight());
             let scapedCopy = await imgScaled.clone();
             let chunk = await scapedCopy.crop(
                 x * PATCH_WIDTH_PX,
@@ -59,14 +43,9 @@ export const processImage = async (buffer: Buffer, width: number, height: number
                 PATCH_WIDTH_PX,
                 PATCH_HEIGHT_PX);
             let chunkData = await chunk.getBufferAsync("image/gif");
-            console.log(chunkData.length);
-            await writeFile("/tmp/img_chunk_x" + x + "_y" + y + ".gif", chunkData);
             chunks[y].push(chunkData);
         }
     }
 
-    console.log("Chunky Bois: ", chunks);
-
-    // console.log("Result: ", result);
-
+    return chunks;
 }
