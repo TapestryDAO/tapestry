af803a20181fb7b7914c4ab66fbc5f99449ab4b0
William Barksdale <wfbarksdale@gmail.com>
2022-01-28 22:19:39 -0500

    test passes

---
+++ place/program/tests/integrations.rs
@@ -0,0 +1,9 @@
+use solana_program::program_error::ProgramError;
+use solana_program_test::{
+    tokio, ProgramTest,
+};
+
+#[tokio::test]
+async fn test_purchase_account() {
+    assert_eq!(true, true);
+}
