e79be24cb828a9c45d98f4ae9262ac875a194ce3
William Barksdale <wfbarksdale@gmail.com>
2022-01-30 04:38:43 -0500

    render loop setup

--- place/app/src/App.tsx
+++ place/app/src/App.tsx
@@ -1,11 +1,42 @@
 import React, { FC, useEffect, useRef } from 'react';
 import { PlaceClient } from '@tapestrydao/place-client'
 
+const DRAW_RATE_MS = 1000 / 10
+
 export const TapestryCanvas: FC = (props) => {
     let thing = PlaceClient.getInstance();
     thing.subscribeToPatchUpdates();
 
     const canvasRef = useRef(null);
+    const animateRequestRef = useRef<number>(null);
+    const previousTimeRef = useRef<number>(null);
+
+    const animate = (time: number) => {
+        // put image data
+        const shouldDraw = previousTimeRef.current === null
+            || time - previousTimeRef.current > DRAW_RATE_MS;
+
+        if (!shouldDraw) {
+            animateRequestRef.current = requestAnimationFrame(animate);
+            return;
+        }
+
+        console.log("DRAW!");
+
+        previousTimeRef.current = time;
+        animateRequestRef.current = requestAnimationFrame(animate);
+    }
+
+    useEffect(() => {
+
+        animateRequestRef.current = requestAnimationFrame(animate)
+
+        return () => {
+            if (animateRequestRef.current !== null) {
+                cancelAnimationFrame(animateRequestRef.current);
+            }
+        }
+    }, []);
 
     useEffect(() => {
         const canvas = canvasRef.current; // TODO(will): how to cast?
@@ -20,7 +51,7 @@ export const TapestryCanvas: FC = (props) => {
         <div style={{ scale: "1,1" }} >
             <canvas ref={canvasRef} width={1920} height={1080} />
         </div>
-    }
+    );
 }
 
 export const App: FC = () => {
