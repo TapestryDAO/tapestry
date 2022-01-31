fa2de82fae4f0f47270a82173529ae8fcbd4d512
William Barksdale <wfbarksdale@gmail.com>
2022-01-31 15:34:58 -0500

    i dunno what i did but its working

--- place/app/src/App.tsx
+++ place/app/src/App.tsx
@@ -1,11 +1,19 @@
 import React, { FC, useEffect, useRef, useState } from 'react';
 import { PlaceClient } from '@tapestrydao/place-client'
 
-const DRAW_RATE_MS = 1000 / 10
-const MAX_SCALE = 40
-const MIN_SCALE = 0.5
+const DRAW_RATE_MS = 1000 / 10;
+const MAX_SCALE = 40;
+const MIN_SCALE = 0.5;
 const PLACE_WIDTH = 1000;
 const PLACE_HEIGHT = 1000;
+
+type TranslationPos = {
+    oldX: number,
+    oldY: number,
+    x: number,
+    y: number,
+    z: number,
+};
 
 export const TapestryCanvas: FC = (props) => {
     let thing = PlaceClient.getInstance();
@@ -18,7 +26,16 @@ export const TapestryCanvas: FC = (props) => {
 
     const [scale, setScale] = useState<number>(1);
     const [canvasTranslation, setCanvasTranslation] = useState<number[]>([0, 0]);
-    const [isDraggingCanvas, setIsDraggingCanvas] = useState<boolean>(false);
+
+    const containerRef = useRef(null);
+    const [isPanning, setIsPanning] = useState<boolean>(false);
+    const [transPos, setTransPos] = useState<TranslationPos>({
+        oldX: 0,
+        oldY: 0,
+        x: 0,
+        y: 0,
+        z: 1,
+    })
 
     const animate = (time: number) => {
         // put image data
@@ -91,11 +108,11 @@ export const TapestryCanvas: FC = (props) => {
     const onMouseDown = (event: MouseEvent) => {
         console.log("mouse down");
         prevMousePos.current = [event.x, event.y];
-        setIsDraggingCanvas(true);
+        setIsPanning(true);
     }
 
     const onMouseMove = (event: MouseEvent) => {
-        if (!isDraggingCanvas) return;
+        if (!isPanning) return;
 
         const scaledMovementX = event.movementX / scale;
         const scaledMovementY = event.movementY / scale;
@@ -111,27 +128,45 @@ export const TapestryCanvas: FC = (props) => {
 
     const onMouseUp = (event: MouseEvent) => {
         console.log("mouse up");
-        setIsDraggingCanvas(false);
+        setIsPanning(false);
     }
 
     return (
-        <div style={{ transform: "scale(" + scale + "," + scale + ")" }} >
-            <div style={{ transform: "translate(" + canvasTranslation[0] + "px," + canvasTranslation[1] + "px)" }} >
-                <canvas
-                    ref={canvasRef}
-                    width={PLACE_WIDTH}
-                    height={PLACE_HEIGHT}
-                    onWheel={onMouseWheel}
-                    onMouseDown={onMouseDown}
-                    onMouseUp={onMouseUp}
-                    onMouseMove={onMouseMove}
-                    onMouseLeave={onMouseUp}
-                    style={{
-                        imageRendering: "pixelated"
-                    }} />
+        <div className="TapestryCanvas_Container">
+            <div style={{
+                transform: "scale(" + scale + "," + scale + ")",
+                width: PLACE_WIDTH,
+                height: PLACE_HEIGHT,
+            }} >
+                <div style={{
+                    transform: "translate(" + canvasTranslation[0] + "px," + canvasTranslation[1] + "px)",
+                    width: PLACE_WIDTH,
+                    height: PLACE_HEIGHT,
+                }} >
+                    <canvas
+                        ref={canvasRef}
+                        width={PLACE_WIDTH}
+                        height={PLACE_HEIGHT}
+                        onWheel={onMouseWheel}
+                        onMouseDown={onMouseDown}
+                        onMouseUp={onMouseUp}
+                        onMouseMove={onMouseMove}
+                        onMouseLeave={onMouseUp}
+                        style={{
+                            imageRendering: "pixelated"
+                        }} />
+                </div>
             </div>
         </div>
     );
+}
+
+export const Sidebar: FC = () => {
+
+    return <div className='Sidebar'
+        style={{
+            backgroundColor: "#EEE"
+        }}></div>
 }
 
 export const App: FC = () => {
